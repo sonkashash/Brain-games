7be15ec8c60cc273faa1c9ccdb02ccbb2a8b652a
Sofia <sophia.shashova@yandex.ru>
2024-05-22 17:37:28 +0400

    Correct errors by linter

--- src/isEven.js
+++ src/isEven.js
@@ -1,17 +1,17 @@
-import readlineSync from "readline-sync";
+import readlineSync from 'readline-sync';
 
 const isEven = (name) => {
   console.log('Answer "yes" if the number is even, otherwise answer "no".');
   for (let i = 0; i < 3; i += 1) {
     const randomNumber = Math.ceil(Math.random() * 100);
     console.log(`Question: ${randomNumber}`);
-    const rightAnswer = randomNumber % 2 ? "no" : "yes";
-    const userAnswer = readlineSync.question("Your answer: ");
+    const rightAnswer = randomNumber % 2 ? 'no' : 'yes';
+    const userAnswer = readlineSync.question('Your answer: ');
     if (rightAnswer === userAnswer) {
-      console.log("Correct!");
+      console.log('Correct!');
     } else {
       console.log(
-        `'${userAnswer}' is wrong answer ;(. Correct answer was '${rightAnswer}'.\nLet's try again, ${name}!`
+        `'${userAnswer}' is wrong answer ;(. Correct answer was '${rightAnswer}'.\nLet's try again, ${name}!`,
       );
       return;
     }
